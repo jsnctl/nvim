b6d3559e26036ab5663bc98563e0a5c659da35fe
Jason Costello <jason@jsnc.tl>
2024-05-19 13:24:42 +0100

    Adding carriage return autocomplete

--- lua/jsnctl/remap.lua
+++ lua/jsnctl/remap.lua
@@ -1,2 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+local cmp = require('cmp')
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+  }),
+})
