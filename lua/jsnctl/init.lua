4da58e37b6c65fcdf06c9e56aa39f2433ef2444a
Jason Costello <jason@cofide.io>
2024-12-16 13:44:18 +0000

    Global clipboard (at last!)

--- lua/jsnctl/init.lua
+++ lua/jsnctl/init.lua
@@ -5,5 +5,6 @@ vim.cmd [[colorscheme evening]]
 vim.api.nvim_set_hl(0, "Normal", {guibg=NONE, ctermbg=NONE})
 vim.api.nvim_set_hl(0, "NonText", {guibg=NONE, ctermbg=NONE})
 vim.api.nvim_set_hl(0, "EndOfBuffer", {guibg=NONE, ctermbg=NONE})
+vim.api.nvim_set_option("clipboard", "unnamed")
 
 require("jsnctl.packer")
