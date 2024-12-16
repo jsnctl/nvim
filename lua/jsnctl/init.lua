1c824333ec1a57fb808a6988439acf2e12cf1ef4
Jason Costello <jason@cofide.io>
2024-12-16 13:46:53 +0000

    Using unnamedplus clipboard

--- lua/jsnctl/init.lua
+++ lua/jsnctl/init.lua
@@ -5,6 +5,6 @@ vim.cmd [[colorscheme evening]]
 vim.api.nvim_set_hl(0, "Normal", {guibg=NONE, ctermbg=NONE})
 vim.api.nvim_set_hl(0, "NonText", {guibg=NONE, ctermbg=NONE})
 vim.api.nvim_set_hl(0, "EndOfBuffer", {guibg=NONE, ctermbg=NONE})
-vim.api.nvim_set_option("clipboard", "unnamed")
+vim.api.nvim_set_option("clipboard", "unnamedplus")
 
 require("jsnctl.packer")
