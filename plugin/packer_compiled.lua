ac9cd4b3d1c92a3c623e6ca165db1fc67a2852f6
Jason Costello <jason@cofide.io>
2025-05-26 11:03:27 +0100

    Ensuring config is up to date

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -134,6 +134,11 @@ _G.packer_plugins = {
     path = "/home/jsnctl/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
   },
+  ["vim-gitgutter"] = {
+    loaded = true,
+    path = "/home/jsnctl/.local/share/nvim/site/pack/packer/start/vim-gitgutter",
+    url = "https://github.com/airblade/vim-gitgutter"
+  },
   ["vim-go"] = {
     loaded = true,
     path = "/home/jsnctl/.local/share/nvim/site/pack/packer/start/vim-go",
