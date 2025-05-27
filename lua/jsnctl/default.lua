3aa15d7e285eda5ec5be99e64302eb7b72d61519
Jason Costello <jason@cofide.io>
2025-05-27 23:20:15 +0100

    Various adjustments

--- lua/jsnctl/default.lua
+++ lua/jsnctl/default.lua
@@ -15,3 +15,19 @@ cmp.setup({
     ['<CR>'] = cmp.mapping.confirm({select = false}),
   }),
 })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.go",
+    callback = function()
+        local params = vim.lsp.util.make_range_params()
+        params.context = { only = { "source.organizeImports" } }
+        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 1000)
+        for _, res in pairs(result or {}) do
+            for _, action in pairs(res.result or {}) do
+                if action.edit then
+                    vim.lsp.util.apply_workspace_edit(action.edit, "utf-8")
+                end
+            end
+        end
+    end,
+})
